4d0e5724a52f9f2bad8cedea78f91350092f69e6
lguziak <lexgzk123@gmail.com>
2023-05-02 12:39:32 -0500

    Developing Ordering - Domain and Application in DDD

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Persitence/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using Ordering.Domain.Entities;
+
+
+namespace Ordering.Application.Contracts.Persitence
+{
+    public interface IOrderRepository : IAsyncRepository<Order>
+    {
+        public interface IOrderRepository : IAsyncRepository<Order>
+        {
+            Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
+        }
+    }
+}
